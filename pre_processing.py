bca00404880531772a2cdbee3b1223f149ed43ae
DeividSkii <darlandeivid64@gmail.com>
2026-05-09 15:49:29 -0300

    colocando função limpar_descricao fora da função carregar_e_limpar_dados para melhor organização

--- pre_processing.py
+++ pre_processing.py
@@ -1,28 +1,28 @@
 import pandas as pd
 
+#Lista as marcas para remover
+marcas_para_remover = ['nike', 'adidas', 'puma', 'lacoste', 'gucci', 'polo', 'micol', 'pimpolho', 'luziane', 'bilu', 'nikko', 'stylo']
+
+# Função para a limpeza do df
+def limpar_descricao(texto):
+    # Garante que o valor é string e converte tudo para minúsculo para padronizar
+    texto = str(texto).lower()
+
+    # Percorre a lista de marcas, se ela existir no texto, substitui ele por 'nada'
+    for marca in marcas_para_remover:
+        texto = texto.replace(marca, '')
+
+    # O split() separa as palavras e o join() juntas apenas com um espaço. Isso remove espaços duplos ou em branco que sobraram onde as marcas foram apagas
+    return ' '.join(texto.split())
 
 def carregar_e_limpar_dados(caminho_arquivo):
 
     #Carrega o dataset
     df = pd.read_csv(caminho_arquivo)
 
-    #Remover linhas nulas, evitando que o algoritmo quebre
+    #Remover linhas nulas
     df = df.dropna(subset=['descricao_produtos', 'id_transacao'])
 
-    #Lista as marcas para remover
-    marcas_para_remover = ['nike', 'adidas', 'puma', 'lacoste', 'gucci', 'polo', 'micol', 'pimpolho', 'luziane', 'bilu', 'nikko', 'stylo']
-
-    #Função para a limpeza do df
-    def limpar_descricao(texto):
-        #Garante que o valor é string e converte tudo para minúsculo para padronizar
-        texto = str(texto).lower()
-
-        #Percorre a lista de marcas, se ela existir no texto, substitui ele por 'nada'
-        for marca in marcas_para_remover:
-            texto = texto.replace(marca, '')
-
-        #O split() separa as palavras e o join() juntas apenas com um espaço. Isso remove espaços duplos ou em branco que sobraram onde as marcas foram apagas
-        return ' '.join(texto.split())
 
     #Aplica a limpeza, pega a coluna original, passa cada linha da função limpar_descricao e guarda o resultado na nova coluna
     df['descricao_limpa'] = df['descricao_produtos'].apply(limpar_descricao)
@@ -35,3 +35,5 @@ def carregar_e_limpar_dados(caminho_arquivo):
 
     #Pega a coluna 'lista_produto' e converte do formato Pandas para uma lista pura do Python
     lista_de_vendas = df['lista_produto'].tolist()
+
+    return lista_de_vendas
